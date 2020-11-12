973297d7cbdb09b265e2c7c570e5890c525be723
Nadav Ashkenazi <nadavash@campus.technion.ac.il>
2020-11-12 12:50:33 +0200

    init commit

---
+++ cmake-build-debug/CMakeFiles/ca_hw1.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/nadav ashkenazi/OneDrive - Technion/Technion/semester_6/Computer Architecture/hw/hw1/ca_hw1/main.cpp" "C:/Users/nadav ashkenazi/OneDrive - Technion/Technion/semester_6/Computer Architecture/hw/hw1/ca_hw1/cmake-build-debug/CMakeFiles/ca_hw1.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/ca_hw1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ca_hw1.dir/main.cpp.obj"
+  "ca_hw1.exe"
+  "ca_hw1.exe.manifest"
+  "ca_hw1.lib"
+  "ca_hw1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ca_hw1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
